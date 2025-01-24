5962a35d6368c1f697cec2dcfdcf2d4fcffca611
Charan <charankumarkharade2003@gmail.com>
2025-01-24 12:26:20 +0530

    Stuck with Paytm

--- shop_app/admin.py
+++ shop_app/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 #register the models
 admin.site.register(models.Contact)
 admin.site.register(models.Product)
+admin.site.register(models.Orders)
+admin.site.register(models.OrderUpdate)
--- shop_app/urls.py
+++ shop_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('about/',views.about,name="about"),
     path('contact/',views.contact,name="contact"),
+    path('checkout/', views.checkout, name="Checkout"),
+     path('handlerequest/', views.handlerequest, name="HandleRequest"),
 ]
